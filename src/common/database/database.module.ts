83a783a6926f5a156b13613ad430700ab6452582
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-29 13:14:52 -0300

    feat: Implementa o repositorio customer ao modulo de database

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DrizzleModule } from './drizzle.module';
+
+import { RequestContextModule } from '../modules/request-context/request-context.module';
 import { DatabaseConfig } from './configs/database.config';
+import { DrizzleModule } from './drizzle.module';
+import { CustomerRepository } from './repositories/customer.repository';
 import { UserRepository } from './repositories/user.repository';
 
-const Repositories = [UserRepository];
+const Repositories = [UserRepository, CustomerRepository];
 
 @Module({
-  providers: [...Repositories, DatabaseConfig],
   imports: [
     ConfigModule.forRoot({
       validate: () => DatabaseConfig.credentials,
     }),
+    RequestContextModule,
     DrizzleModule,
   ],
+  providers: [...Repositories, DatabaseConfig],
   exports: Repositories,
 })
 export class DatabaseModule {}
