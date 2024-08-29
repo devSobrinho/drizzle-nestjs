6ef3900a5fe64e92fc4d1e8e92efc21cef4fb20e
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-29 20:17:48 -0300

    feat: Adicionar o swagger ao app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,14 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('DRIZZLE API')
+    .setDescription('The DRIZZLE API description')
+    .setVersion('1.0')
+    .addTag('drizzle')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
