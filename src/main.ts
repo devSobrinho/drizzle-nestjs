e7ac63bd1a1d35a7baa55589ab9f90ca1f46aa5b
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-29 03:29:20 -0300

    styles: Remover import não utilizado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import './common/drizzle/database/schemas/main';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
