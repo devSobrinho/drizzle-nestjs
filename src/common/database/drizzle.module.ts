70285e86620335cd593d71e78d53436e2abb388b
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-29 13:15:50 -0300

    feat: Implementa o modulo de request context para pegar o schema do usuario quando logado no sistema e injetar na conexao seu schema

--- src/common/database/drizzle.module.ts
+++ src/common/database/drizzle.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { sql } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
+import { RequestContextModule } from '../modules/request-context/request-context.module';
+import { RequestContextService } from '../modules/request-context/request-context.service';
 import { DatabaseConfig } from './configs/database.config';
 import { EntitiesSchema } from './entities/entities.schema';
 import { PG_CONNECTION } from './pg-connection';
@@ -12,16 +15,25 @@ import { PG_CONNECTION } from './pg-connection';
     ConfigModule.forRoot({
       validate: () => DatabaseConfig.credentials,
     }),
+    RequestContextModule,
   ],
   providers: [
     DatabaseConfig,
     {
       provide: PG_CONNECTION,
-      inject: [DatabaseConfig],
-      useFactory: async (dbConfig: DatabaseConfig) => {
+      inject: [DatabaseConfig, RequestContextService],
+      useFactory: async (
+        dbConfig: DatabaseConfig,
+        requestContextService: RequestContextService,
+      ) => {
         const pool = new Pool({
           connectionString: dbConfig.postgressqlConnection,
         });
+        const db = drizzle(pool, { schema: EntitiesSchema });
+        let schemaName = dbConfig.schemaName;
+        const user = requestContextService.getUser();
+        if (user?.tenantId) schemaName = `tenant_${user.tenantId}`;
+        await db.execute(sql.raw(`SET schema '${schemaName}'`));
         return drizzle(pool, { schema: EntitiesSchema });
       },
     },
