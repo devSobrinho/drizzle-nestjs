ad6665515430559c7f46d166ea514e44a6425605
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-29 13:19:14 -0300

    feat: Implementar middleware e jwt-auth ao app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { RequestContextMiddleware } from './common/middlewares/request-context.middleware';
+import { RequestContextModule } from './common/modules/request-context/request-context.module';
 import { UserModule } from './modules/user/user.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule, UserModule],
+  imports: [ConfigModule, RequestContextModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestContextMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
