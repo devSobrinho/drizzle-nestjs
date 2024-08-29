559c15071e5ff55a65694fb1ee9bd439f31d1b01
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-29 05:51:48 -0300

    feat: Adiciona os modulos de Config e User ao app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
