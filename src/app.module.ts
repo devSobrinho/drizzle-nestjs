6affd92ba0091ca52d96f4aa43c71d202a98c76a
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-08-31 15:02:55 -0300

    feat: Adicionar módulos para o app

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,22 @@ import { AppService } from './app.service';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { RequestContextMiddleware } from './common/middlewares/request-context.middleware';
 import { RequestContextModule } from './common/modules/request-context/request-context.module';
+import { UploadModule } from './modules/upload/upload.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { MailModule } from './modules/mail/mail.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [ConfigModule, RequestContextModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventEmitterModule.forRoot(),
+    RequestContextModule,
+    UploadModule,
+    UserModule,
+    AuthModule,
+    MailModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
