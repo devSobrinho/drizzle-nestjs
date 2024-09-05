74498d40468e3062c8938e7d14ea278d4b5a1dbd
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-09-04 22:25:52 -0300

    feat: Atualizar entidade de endereço com campos adicionais

--- src/common/database/entities/tenant/address.entity.ts
+++ src/common/database/entities/tenant/address.entity.ts
@@ -4,12 +4,26 @@ import { order } from './order.entity';
 import { BaseEntityHelper } from '../../helpers/base-entity.helper';
 
 // ------- ADDRESS TABLE ---------
-export const address = d.pgTable('address', {
-  ...BaseEntityHelper.idPrimaryKey,
-  name: d.varchar('name', { length: 256 }).notNull(),
-  description: d.varchar('description', { length: 256 }).notNull(),
-  ...BaseEntityHelper.timestampColumns,
-});
+export const address = d.pgTable(
+  'address',
+  {
+    ...BaseEntityHelper.idPrimaryKey,
+    street: d.varchar('street', { length: 255 }).notNull(),
+    city: d.varchar('city', { length: 255 }).notNull(),
+    state: d.varchar('state', { length: 2 }).notNull(),
+    country: d.varchar('country', { length: 255 }).notNull(),
+    postalCode: d.varchar('postal_code', { length: 20 }).notNull(),
+    neighborhood: d.varchar('neighborhood', { length: 255 }).notNull(),
+    ...BaseEntityHelper.timestampColumns,
+  },
+  (table) => {
+    return {
+      postalCodeIdx: d
+        .uniqueIndex('address_postal_code_idx')
+        .on(table.postalCode),
+    };
+  },
+);
 
 // RELATIONS
 export const addressRelations = relations(address, ({ many }) => ({
