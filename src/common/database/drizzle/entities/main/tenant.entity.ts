b97528eb0d0be1c96c5d63f3897b46f18613f165
Daniel Sobrinho <dev.sobrinho@gmail.com>
2024-09-07 13:10:08 -0300

    feat: Atualizar entidade

--- src/common/database/drizzle/entities/main/tenant.entity.ts
+++ src/common/database/drizzle/entities/main/tenant.entity.ts
@@ -7,20 +7,26 @@ import { BaseEntityHelper } from '../../helpers/base-entity.helper';
 const schemaMain = d.pgSchema('main');
 
 // ------- TENANT TABLE ---------
-export const tenant = schemaMain.table('tenant', {
-  id: d.serial('id').primaryKey(),
-  name: d.varchar('name', { length: 256 }).notNull(),
-  cep: d.varchar('cep', { length: 256 }).notNull(),
-  address: d.varchar('address', { length: 256 }).notNull(),
-  number: d.varchar('number').notNull(),
-  city: d.varchar('city', { length: 256 }).notNull(),
-  state: d.varchar('state', { length: 256 }).notNull(),
-  country: d.varchar('country', { length: 256 }).notNull(),
-  phone: d.varchar('phone', { length: 256 }).notNull(),
-  email: d.varchar('email', { length: 256 }).notNull(),
-  cnpj: d.varchar('cnpj', { length: 256 }).notNull(),
-  ...BaseEntityHelper.timestampColumns,
-});
+export const tenant = schemaMain.table(
+  'tenant',
+  {
+    id: d.serial('id').primaryKey(),
+    name: d.varchar('name', { length: 256 }).notNull(),
+    cep: d.varchar('cep', { length: 256 }).notNull(),
+    address: d.varchar('address', { length: 256 }).notNull(),
+    number: d.varchar('number').notNull(),
+    city: d.varchar('city', { length: 256 }).notNull(),
+    state: d.varchar('state', { length: 256 }).notNull(),
+    country: d.varchar('country', { length: 256 }).notNull(),
+    phone: d.varchar('phone', { length: 256 }).notNull(),
+    email: d.varchar('email', { length: 256 }).notNull(),
+    cnpj: d.varchar('cnpj', { length: 256 }).notNull(),
+    ...BaseEntityHelper.timestampColumns,
+  },
+  (table) => ({
+    emailIdx: d.uniqueIndex('email_tenant_id_idx').on(table.id, table.email),
+  }),
+);
 
 // RELATIONS
 export const tenantRelations = relations(tenant, ({ many }) => ({
